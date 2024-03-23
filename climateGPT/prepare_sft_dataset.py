15337ef5f7b4380969dd63ed6d9de2edd677304f
pierre <pierre.pessarossi@gmail.com>
2024-03-23 11:04:46 +0100

    Push instruction dataset to huggingface hub
    The new class loads clean question/answer dataset, perform a train/test split
    and push the dataset to huggingface hub.

--- climateGPT/prepare_sft_dataset.py
+++ climateGPT/prepare_sft_dataset.py
@@ -1,8 +1,10 @@
 import hashlib
 import os
+import random
 from dataclasses import dataclass
 from pathlib import Path
 
+import datasets
 import pandas as pd
 import structlog
 from datasets import load_dataset
@@ -34,6 +36,15 @@ class PrepareDataConfig:
     dataset_hf_address: str = DATASET_HF_ADDRESS
     local_cache: str = LOCAL_CACHE
     max_range: int = -1
+
+
+@dataclass
+class PushToHuggingFaceHubConfig:
+    local_folder: str
+    user_name_on_huggingface: str
+    dataset_name_on_huggingface: str
+    random_seed: int = 498
+    test_split: float = 0.2
 
 
 def create_key_from_title(title: str) -> str:
@@ -132,17 +143,104 @@ class PrepareSFTDataset:
             self.create_instruction_answer(i)
 
 
+class PushDatasetToHuggingFaceHub:
+    def __init__(
+        self,
+        local_folder: str,
+        user_name_on_huggingface: str,
+        dataset_name_on_huggingface: str,
+        random_seed: int = 498,
+        test_split: float = 0.2,
+    ) -> None:
+        self.local_folder = Path(local_folder)
+        self.files = [file.name for file in self.local_folder.glob("*.json")]
+        self.user_name_on_huggingface = user_name_on_huggingface
+        self.dataset_name_on_huggingface = dataset_name_on_huggingface
+        self.random_seed = random_seed
+        self.test_split = test_split
+
+    def load_data_in_pandas_df(self) -> None:
+        self.df = pd.concat(
+            [pd.read_json(self.local_folder / file, lines=True) for file in self.files],
+            axis=0,
+            ignore_index=True,
+        )
+        log.info(f"Pandas dataframe loaded of shape: {self.df.shape}")
+
+    def perform_train_test_split(self) -> None:
+        full_index = self.df.index.to_list()
+        random.seed(self.random_seed)
+        random.shuffle(full_index)
+        test_size = int(self.test_split * len(self.df))
+        self.test_index = full_index[:test_size]
+        self.train_index = full_index[test_size:]
+        test_share = len(self.test_index) / (
+            len(self.train_index) + len(self.test_index)
+        )
+        log.info(f"""Test represents {test_share:.2%} of full dataset""")
+
+    def convert_to_hf_dataset(self) -> None:
+        self.huggingface_dataset = datasets.DatasetDict(
+            {
+                "train": datasets.Dataset.from_pandas(self.df.loc[self.train_index]),
+                "test": datasets.Dataset.from_pandas(self.df.loc[self.test_index]),
+            }
+        )
+        log.info("Dataset converted to HuggingFace dataset dict.")
+
+    def push_to_huggingface_hub(self):
+        self.huggingface_dataset.push_to_hub(
+            f"{self.user_name_on_huggingface}/{self.dataset_name_on_huggingface}"
+        )
+        log.info("Dataset pushed to Hugging Face Hub")
+
+    def __call__(self) -> None:
+        self.load_data_in_pandas_df()
+        self.perform_train_test_split()
+        self.convert_to_hf_dataset()
+        self.push_to_huggingface_hub()
+
+
 if __name__ == "__main__":
-    parser = ArgumentParser(description="Create a dataset for SFT of an LLM.")
-    parser.add_arguments(PrepareDataConfig, dest="data_config")
+    parser = ArgumentParser(
+        description="Create a dataset for SFT of an LLM. Push it to Huggingface Hub"
+    )
+    subparsers = parser.add_subparsers(
+        dest="task",
+        help="Create dataset for SFT or push it to Huggingface Hub",
+    )
+    parser_create_dataset = subparsers.add_parser(
+        "create-dataset", help="Prepares and save locally a dataset for SFT"
+    )
+    parser_create_dataset.add_arguments(PrepareDataConfig, dest="data_config")
+
+    parser_push_to_hub = subparsers.add_parser(
+        "push-to-hub", help="Push a clean instruction dataset to hub"
+    )
+    parser_push_to_hub.add_arguments(PushToHuggingFaceHubConfig, dest="push_config")
 
     args = parser.parse_args()
 
-    prepare_sft_dataset = PrepareSFTDataset(
-        dataset_hf_address=args.data_config.dataset_hf_address,
-        model_name=args.data_config.model_name,
-        prompt_template_name=args.data_config.prompt_template_name,
-        local_cache=args.data_config.local_cache,
-    )
+    if args.task == "create-dataset":
+        prepare_sft_dataset = PrepareSFTDataset(
+            dataset_hf_address=args.data_config.dataset_hf_address,
+            model_name=args.data_config.model_name,
+            prompt_template_name=args.data_config.prompt_template_name,
+            local_cache=args.data_config.local_cache,
+        )
 
-    prepare_sft_dataset(max_range=args.data_config.max_range)
+        prepare_sft_dataset(max_range=args.data_config.max_range)
+
+    elif args.task == "push-to-hub":
+        push_dataset_to_huggingface_hub = PushDatasetToHuggingFaceHub(
+            local_folder=args.push_config.local_folder,
+            user_name_on_huggingface=args.push_config.user_name_on_huggingface,
+            dataset_name_on_huggingface=args.push_config.dataset_name_on_huggingface,
+            random_seed=args.push_config.random_seed,
+            test_split=args.push_config.test_split,
+        )
+
+        push_dataset_to_huggingface_hub()
+
+
+# TODO: chunk article when too big
